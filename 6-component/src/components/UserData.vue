7c71c2ba7c3443c3ddfd058d026f315416f72963
Dany Sentiana <danysentiana1200@gmail.com>
2023-05-20 21:54:05 +0700

    Learn Vue Component

---
+++ 6-component/src/components/UserData.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="addUserData">
+    <label>Username</label>
+    <input type="text" v-model="userName" />
+    <label>Age</label>
+    <input type="text" v-model="userAge" />
+    <button type="submit">Add User Data</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-user-data"],
+  data() {
+    return {
+      userName: "",
+      userAge: "",
+    };
+  },
+  methods: {
+    addUserData() {
+      this.$emit("add-user-data", {
+        name: this.userName,
+        age: this.userAge,
+      });
+      this.userName = "";
+      this.userAge = "";
+    },
+  },
+};
+</script>
